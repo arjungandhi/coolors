8f53446c66a4df862221496ed91e3b7f552ae049
Arjun Gandhi <account@arjugandhi.com>
2021-06-16 18:32:09 -0400

    updated to poetry

--- main.py
+++ main.py
@@ -2,21 +2,16 @@ import asyncio
 from pyppeteer import launch
 import json
 
-def lambda_handler(event, context):
+def gen_colors():
     loop = asyncio.get_event_loop()
-    r = loop.run_until_complete(main())
+    r = loop.run_until_complete(_main())
     result = r.split('/')[-1]
-    result = result.split('-')
+    results = result.split('-')
 
-    print(result)
+    return results
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps(result)
-    }
-
-async def main():
-    browser = await launch(headless=True, args= ['--no-sandbox'] )
+async def _main():
+    browser = await launch(headless=True)
     page = await browser.newPage()
     await page.goto('https://coolors.co/generate',{'waitUntil' : 'networkidle2'})
     js = "() => { return window.location.href }"
