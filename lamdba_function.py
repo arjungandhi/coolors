238e6eee83e59fd461f4989f9d164885ede4f68b
Arjun Gandhi <account@arjugandhi.com>
2021-06-16 13:29:30 -0400

    converted to lambda function

--- lamdba_function.py
+++ lamdba_function.py
@@ -6,6 +6,8 @@ def lambda_handler(event, context):
     loop = asyncio.get_event_loop()
     r = loop.run_until_complete(main())
     result = r.split('/')[-1]
+    result = result.split('-')
+
     print(result)
 
     return {
@@ -16,11 +18,9 @@ def lambda_handler(event, context):
 async def main():
     browser = await launch(headless=True)
     page = await browser.newPage()
-    await page.goto('https://coolors.co/generate',{'waitUntil' : 'networkidle0'})
+    await page.goto('https://coolors.co/generate',{'waitUntil' : 'networkidle2'})
     js = "() => { return window.location.href }"
     result = await page.evaluate(js)
     await browser.close()
 
     return result
-
-lambda_handler(None,None)
